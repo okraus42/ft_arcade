70680edbc116684cfe99f9e511491624f0132896
okraus <okraus42+github@pm.me>
2025-05-06 07:06:18 +0200

    format main

--- tetris/src/main.c
+++ tetris/src/main.c
@@ -4,7 +4,7 @@
 int main(void)
 {
 	t_game game;
-	bool running = true;
+	bool   running = true;
 	init_game(&game);
 	uint32_t current_time = SDL_GetTicks();
 	while (running)
@@ -16,14 +16,16 @@ int main(void)
 			update_game_1(&game); // Call your function
 		if (game.tetris[PLAYER_2].running && game.frame >= game.tetris[PLAYER_2].next_move)
 			update_game_2(&game); // Call your function
-		update_render(&game); // Call your function
+		update_render(&game);	  // Call your function
 		render(&game);
 		if (running)
 			running = game.tetris[PLAYER_1].running || game.tetris[PLAYER_2].running;
 		SDL_Delay(16); // ~60 FPS
 		game.frame += 1;
 	}
-	printf("Player 1: %8u %3u %4u\nPlayer 2: %8u %3u %4u\n", game.tetris[PLAYER_1].score, game.tetris[PLAYER_1].level, game.tetris[PLAYER_1].lines, game.tetris[PLAYER_2].score, game.tetris[PLAYER_2].level, game.tetris[PLAYER_2].lines);
+	printf("Player 1: %8u %3u %4u\nPlayer 2: %8u %3u %4u\n", game.tetris[PLAYER_1].score, game.tetris[PLAYER_1].level,
+		   game.tetris[PLAYER_1].lines, game.tetris[PLAYER_2].score, game.tetris[PLAYER_2].level,
+		   game.tetris[PLAYER_2].lines);
 	cleanup(&game);
 	return 0;
 }
